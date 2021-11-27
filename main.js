216312f9b3417302d43825a104585f1f6a381323
Olivia-Charrois <77928362+Olivia-Charrois@users.noreply.github.com>
2021-11-27 10:26:01 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+song = "";
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+function setup(){
+ canvas = createCanvas(400, 400);
+ canvas.center();
+
+ video = createCapture(VIDEO);
+ video.hide();
+
+ poseNet = ml5.poseNet(video, modelLoaded);
+ poseNet.on('pose', gotPoses);
+}
+
+function preload()
+{
+ song1 = loadSound("music.mp3");
+ song2 = loadSound("music2.mp3");
+}
+
+function modelLoaded(){
+ console.log('PoseNet Is Initialized!');
+}
+
+function draw()
+{
+ image(video,0,0,400,400);
+
+ fill("#FF0000");
+ stroke("#FF0000");
+ 
+ if(scoreLeftWrist > 0.2)
+ {
+  circle(leftWristX, leftWristY,20);
+  song.stop(music2.mp3);
+
+  if(song1 == false)
+  {
+  song = music.mp3;
+  document.getElementById("song_name").innerHTML = "Harry Potter Theme Song";
+  }
+ }
+
+  if(scoreRightWrist > 0.2)
+  {
+   circle(rightWristX, rightWristY,20);
+   song.stop(music.mp3);
+ 
+   if(song2 == false)
+   {
+   song = music2.mp3;
+   document.getElementById("song_name").innerHTML = "Peter Pan Song";
+   }
+  }
+ }
+
+function play(){
+ song.play();
+ song.setVolume(1);
+ song.rate(1)
+}
+function gotPoses(results)
+{
+ if(results.length > 0)
+ {
+  console.log(results);
+  scoreRightWrist = results[0].pose.keypoints[10].score;
+  scoreLeftWrist = results[0].pose.keypoints[9].score;
+  console.log("scoreRightWrist =" + scoreRightWrist + "scoreLeftWrist =" + scoreLeftWrist);
+
+  leftWristX = results[0].pose.leftWrist.x;
+  leftWristY = results[0].pose.leftWrist.y;
+  console.log("leftWristX = " + leftWristX +"leftWristY ="+ leftWristY);
+
+  rightWristX = results[0].pose.rightWrist.x;
+  righttWristY = results[0].pose.rightWrist.y;
+  console.log("rightWristX = " + rightWristX +"rightWristY ="+ rightWristY);
+ }
+}
